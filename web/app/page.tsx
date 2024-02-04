7ec4ec2a9c873bdcb5d28371036aabc7269c1c65
Tahlil <bsse0803@iit.du.ac.bd>
2024-02-04 21:58:40 +0600

    Detect wallet connection on home page

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -16,63 +16,12 @@ export default function Page() {
       <ClusterProvider>
         <SolanaProvider>
           <UiLayout>
-            <div className="flex flex-col bg-gradient-to-br from-red-300 to-red-900">
-              <div className="flex flex-col h-screen ">
-                {/* <Navbar /> */}
-                <div className="flex h-full">
-                  
-                  <div className="flex h-full items-center px-32">
-                    <img src="/hero.png" alt="kpop" />
-                  </div>
-                  <div className="flex flex-col h-full justify-center pr-12">
-                 
+          <div className="flex flex-col bg-gradient-to-br from-red-300 to-red-900">
+          <div className="flex flex-col h-screen w-screen">
 
-                  <div className="font-extrabold text-3xl md:text-4xl [text-wrap:balance] bg-clip-text text-transparent bg-gradient-to-r from-slate-50 to-50% to-slate-1 p-11">
-                  <img src="/logo.png" alt="kpop" width={330} height={330} />
-               Secure your ticket in a heartbeat for
-               <span className="text-pink-800 inline-flex flex-col h-[calc(theme(fontSize.3xl)*theme(lineHeight.tight))] md:h-[calc(theme(fontSize.4xl)*theme(lineHeight.tight))] overflow-hidden">
-                 <ul className="block animate-text-slide-7 text-center leading-tight [&_li]:block">
-                   <li>Concerts</li>
-                   <li>Sports Games</li>
-                   <li>Cultural Festivals</li>
-                   <li>Conferences</li>
-                   <li>Theater Shows</li>
-                   <li>Movie Premieres</li>
-                   <li>Art Exhibitions</li>
-                   <li>Comedy Nights</li>
-                   <li>Workshops</li>
-                   <li>Food Festivals</li>
-                   <li>Tech Conferences</li>
-                   <li>Outdoor Adventures</li>
-                   <li>Fundraising Galas</li>
-                   <li>Fashion Shows</li>
-                   <li>Charity Event</li>
-                   <li aria-hidden="true">Educational Seminars</li>
-                 </ul>
-               </span>
-             </div>
-                    <p className="uppercase text-4xl font-bold">SBS MTV The KPOP</p>
-                    <p className="uppercase text-4xl font-bold">show ticket package</p>
-                    <p className="text-pretty pt-4">
-                      Look no further! Our SBS The Show Tickets are the simplest way for
-                      you to experience a live Kpop recording.
-                    </p>
-                    <div className='m-3'>
-                    <Home></Home>
-                    </div>
-                   
-                    {/* <div className="flex justify-center items-center py-6 px-3 space-x-4">
-                      <button className="bg-red-700 hover:bg-red-500 text-white font-bold py-2 px-4 rounded-full border border-red-700">
-                        Get ticket
-                      </button>
-                      <button className="rounded-full bg-transparent hover:bg-red-500 text-white-700 font-semibold hover:text-white py-2 px-4 border border-white hover:border-transparent">
-                        Create Event
-                      </button>
-                    </div> */}
-                  </div>
-                </div>
-              </div>
-            </div>
+            <Home></Home>
+          </div>
+          </div>
           </UiLayout>
         </SolanaProvider>
       </ClusterProvider>
--- web/components/home.tsx
+++ web/components/home.tsx
@@ -3,7 +3,55 @@ import { useWallet } from '@solana/wallet-adapter-react';
 
 export default function Home() {
     const { connected } = useWallet();
-    return (
-        <h1 className="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{connected? "Wallet connected" : "Please connect your wallet"}</h1>
-      ) 
+    return connected? (
+     
+     
+          <div className="flex h-full">
+            
+            <div className="flex h-full items-center px-32">
+              <img src="/hero.png" alt="kpop" />
+            </div>
+            <div className="flex flex-col h-full justify-center pr-12">
+           
+            
+              <p className="uppercase text-4xl font-bold">SBS MTV The KPOP</p>
+              <p className="uppercase text-4xl font-bold">show ticket package</p>
+              <p className="text-pretty pt-4">
+                Look no further! Our SBS The Show Tickets are the simplest way for
+                you to experience a live Kpop recording.
+              </p>
+             
+            </div>
+          </div>
+     
+        ) : (
+            <div className="font-extrabold text-3xl md:text-4xl [text-wrap:balance] bg-clip-text text-transparent bg-gradient-to-r from-slate-50 to-50% to-slate-1 p-11">
+            <img src="/logo.png" alt="kpop" width={330} height={330} />
+         Secure your ticket in a heartbeat for
+         <span className="text-pink-800 inline-flex flex-col h-[calc(theme(fontSize.3xl)*theme(lineHeight.tight))] md:h-[calc(theme(fontSize.4xl)*theme(lineHeight.tight))] overflow-hidden">
+           <ul className="block animate-text-slide-7 text-center leading-tight [&_li]:block">
+             <li>Concerts</li>
+             <li>Sports Games</li>
+             <li>Cultural Festivals</li>
+             <li>Conferences</li>
+             <li>Theater Shows</li>
+             <li>Movie Premieres</li>
+             <li>Art Exhibitions</li>
+             <li>Comedy Nights</li>
+             <li>Workshops</li>
+             <li>Food Festivals</li>
+             <li>Tech Conferences</li>
+             <li>Outdoor Adventures</li>
+             <li>Fundraising Galas</li>
+             <li>Fashion Shows</li>
+             <li>Charity Event</li>
+             <li aria-hidden="true">Educational Seminars</li>
+           </ul>
+         </span>
+         <div className='p-5'>
+         <h1 className='inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>Please Connect your wallet</h1>
+
+         </div>
+       </div>
+            )
 }
